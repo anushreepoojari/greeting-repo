82def94e2df98cc3f87c610c7e379b8b41d7945e
anushree <anushreepoojari16@gmail.com>
2026-03-05 22:10:06 +0530

    Added C programs and greeting file

---
+++ p1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Print a message to the screen
+    printf("Hello, World!\n");
+
+    // Simple addition
+    int a = 5;
+    int b = 3;
+    int sum = a + b;
+
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+
+    return 0;
+}
